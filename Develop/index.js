a4b5493316d6cc7c4e5fae48aff16fb7fb203661
Daleray1231 <daleray1231@gmail.com>
2023-10-06 08:45:11 -0500

    added prompt questions

--- Develop/index.js
+++ Develop/index.js
@@ -2,7 +2,48 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // Create an array of questions for user input
-
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'How can someone install your project?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'How should your project be used?',
+  },
+  {
+    type: 'input',
+    name: 'license',
+    message: 'What is the project license?',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'How can others contribute to your project?',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'How can someone run tests on your project?',
+  },
+  {
+    type: 'input',
+    name: 'questions',
+    message: 'How can users reach out to you with questions?',
+  },
+];
 
 // Prompt the user for input
 
