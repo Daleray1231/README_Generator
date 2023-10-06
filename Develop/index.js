9c01b888386f1051247d6500845b74366dcc2457
Daleray1231 <daleray1231@gmail.com>
2023-10-06 08:44:30 -0500

    added necesarry packages

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,10 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Create an array of questions for user input
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Prompt the user for input
 
-// Function call to initialize app
-init();
+
+// Create a function to generate README content
